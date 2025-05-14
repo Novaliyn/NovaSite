375138c0a56dfa75b0ce30c56f412dec6509a920
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-05-14 10:39:38 -0700

    fixed basics

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import flask, render_template
+from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=443)
