136e914833736892ff3decca8dae6afc0b1ff283
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-05-15 09:44:48 -0700

    button to redirect + another page base.

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/rain')
+def rain():
+    return render_template('rain.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=6682)
