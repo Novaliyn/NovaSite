b854cbdf911748347a25c2bb83e9b1e63661ff1d
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-05-15 12:05:37 -0700

    FIXED RAIN

--- static/rain.js
+++ static/rain.js
@@ -23,14 +23,14 @@ function draw() {
     context.fillStyle = "#00ff00";
     context.font = `${fontSize}px monospace`;
 
-    for (let i = 0; i < rain.length; i++) {
+    for (let i = 0; i < rain.length; i += 0.5) {
         const text = characters[Math.floor(Math.random() * characters.length)];
         context.fillText(text, i * fontSize, rain[i] * fontSize);
 
         if (rain[i] * fontSize > canvas.height && Math.random() > 0.975) {
             rain[i] = 0;
         }
-        rain[i]++;
+        rain[i] += 0.5;
     }
 
     requestAnimationFrame(draw);
